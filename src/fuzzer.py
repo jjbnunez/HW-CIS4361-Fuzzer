2e621fb0a75d77c343ca63ef7dcbfb6ac9c4dd2f
Jesse <jesse.spencer@me.com>
2018-04-13 13:14:58 -0400

    Added fuzzer.py and function shells

---
+++ src/fuzzer.py
@@ -0,0 +1,29 @@
+# Mutation-based fuzzer
+#
+# CIS 4361 SP-18
+#
+# - Derek Borges
+# - Stephen Davis
+# - Jorge Nuñez
+# - Jesse Spencer
+
+import subprocess
+
+class Fuzzer(object):
+
+    def __init__(self):
+        pass
+
+    def fuzz(self):
+        pass
+
+    def mutate(self):
+        pass
+
+    def launchProcess(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
